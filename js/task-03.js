9a1582c06a1d7da9bf1c2f08718ee2d04fd611b9
helenserb <serbinenkoifbb@gmail.com>
2022-12-26 16:16:06 +0200

    Third task

--- js/task-03.js
+++ js/task-03.js
@@ -8,20 +8,7 @@
 
 // Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
 // Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
-// const images = [
-//   {
-//     url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?dpr=2&h=750&w=1260",
-//     alt: "White and Black Long Fur Cat",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Orange and White Koi Fish Near Yellow Koi Fish",
-//   },
-//   {
-//     url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?dpr=2&h=750&w=1260",
-//     alt: "Group of Horses Running",
-//   },
-// ];
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -36,3 +23,11 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const listEl = document.querySelector('.gallery');
+
+const createImgEl = images
+  .map(image => `<li><img src=${image.url} alt='${image.alt}'width = 400 height = 300></li>`)
+  .join("");
+
+listEl.insertAdjacentHTML("afterbegin", createImgEl);
