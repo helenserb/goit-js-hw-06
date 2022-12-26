8d9022070d110f96ea503c8c05cb59954f2bb455
helenserb <serbinenkoifbb@gmail.com>
2022-12-26 19:01:50 +0200

    Eighth task

--- js/task-08.js
+++ js/task-08.js
@@ -17,3 +17,22 @@
 // Якщо у формі є незаповнені поля, виводь alert з попередженням про те, що всі поля повинні бути заповнені.
 // Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт, де ім'я поля буде ім'ям властивості, а значення поля - значенням властивості. Для доступу до елементів форми використовуй властивість elements.
 // Виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+const form = document.querySelector(".login-form");
+const email = document.querySelector('input[name="email"]');
+const password = document.querySelector('input[name="password"]');
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  console.log(`Email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+}
